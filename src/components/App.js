c4152821ca1eaa1b1319086761af7b021daf4351
MAGgie <mganant08@gmail.com>
2021-08-19 13:10:39 +0530

    fixed

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,46 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
+function getCurrentTime(){
+    const currentDate = new Date();
+    // const hours = currentDate.getHours();
+    // const mins = currentDate.getMinutes();
+    // const sec = currentDate.getSeconds();
+    // return hours + ':' + mins + ':' + sec;
+
+    //currentDate.setHours(5);
+    //currentDate.setMinutes(3);
+    //currentDate.setSeconds(1);
+
+    return currentDate.toLocaleTimeString().toUpperCase();
+}
+
 class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            time: getCurrentTime(),
+        };
+    }
+
+    componentDidMount(){
+        this.intervalId = setInterval(() => {
+            this.setState({time: getCurrentTime()});
+        }, 1000);
+    }
+
+    componentWillUnmount(){
+        clearInterval(this.intervalId);
+    }
+
     render() {
 
         return(
-            <>
-               
-            </>
+            <div className="Clock">
+               <h3 id="time">
+                {this.state.time}
+               </h3>
+            </div>
         )
     }
 }
